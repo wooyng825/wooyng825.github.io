50acc08090caa745ad87f9e82911de405efee72d
wooyng825 <wooyng825@naver.com>
2023-01-09 22:49:08 +0900

    페이지 레이아웃 + 스크롤업 메뉴 나타내기 (Constructure page layout)

---
+++ assets/js/scroll-up-down.js
@@ -0,0 +1,34 @@
+let lastScrollTop = 0;
+const delta = 5;
+let fixBox = document.querySelector('#header');
+let fixBoxHeight = fixBox.offsetHeight;
+let didScroll;
+//스크롤 이벤트 
+window.onscroll = function(e) {
+    didScroll = true;
+};
+
+//0.25초마다 스크롤 여부 체크하여 스크롤 중이면 hasScrolled() 호출
+setInterval(function(){
+    if(didScroll){
+        hasScrolled();
+        didScroll = false;
+    }
+}, 250);
+
+function hasScrolled(){
+    let nowScrollTop = window.scrollY;
+    if (Math.abs(lastScrollTop - nowScrollTop) <= delta){
+        return;
+    }
+    if (nowScrollTop > lastScrollTop && nowScrollTop > fixBoxHeight){
+        //Scroll down
+        fixBox.classList.remove('show');
+    }else {
+        if (nowScrollTop + window.innerHeight < document.body.offsetHeight){
+            //Scroll up
+            fixBox.classList.add('show');
+        }
+    }
+    lastScrollTop = nowScrollTop;
+}
